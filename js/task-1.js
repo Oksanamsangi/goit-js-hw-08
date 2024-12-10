739809c80c6e1b6140f40eb6e118cc9620b66c09
Oksanamsangi <162930811+Oksanamsangi@users.noreply.github.com>
2024-12-10 02:11:44 -0600

    hw6

--- js/task-1.js
+++ js/task-1.js
@@ -1,19 +1,35 @@
-function isEnoughCapacity(products, containerSize) {
-  let totalProducts = 0;
-  const productValues = Object.values(products);
-  for (let quantity of productValues) {
-    totalProducts += quantity;
-  }
-  if (totalProducts <= containerSize) {
-    return true;
-  } else {
-    return false;
-  }
-}
-console.log(isEnoughCapacity({ apples: 2, grapes: 3, carrots: 1 }, 8)); // true
+// ? Перед звільненням розробник зламав вихідний код управління акаунтами користувачів нашого сервісу доставки їжі.
+// ?Виконай рефакторинг методів об'єкта customer, розставивши відсутні this під час звернення до властивостей об'єкта.
 
-console.log(isEnoughCapacity({ apples: 4, grapes: 6, lime: 16 }, 12)); // false
+// ?Використай цей стартовий код і виконай рефакторинг. Після оголошення об'єкта ми додали виклики методів.
+// ?У консоль будуть виведені результати їх роботи.Будь ласка, нічого там не змінюй.
 
-console.log(isEnoughCapacity({ apples: 1, lime: 5, tomatoes: 3 }, 14)); // true
+const customer = {
+  username: "Mango",
+  balance: 24000,
+  discount: 0.1,
+  orders: ["Burger", "Pizza", "Salad"],
 
-console.log(isEnoughCapacity({ apples: 18, potatoes: 5, oranges: 2 }, 7)); // false
+  getBalance() {
+    return this.balance;
+  },
+  getDiscount() {
+    return this.discount;
+  },
+  setDiscount(value) {
+    this.discount = value;
+  },
+  getOrders() {
+    return this.orders;
+  },
+  addOrder(cost, order) {
+    this.balance -= cost - cost * this.discount;
+    this.orders.push(order);
+  },
+};
+
+customer.setDiscount(0.15);
+console.log(customer.getDiscount()); // 0.15
+customer.addOrder(5000, "Steak");
+console.log(customer.getBalance()); // 19750
+console.log(customer.getOrders()); // ["Burger", "Pizza", "Salad", "Steak"]
